4b9cfca69f4a8ae1006cf742bca17fd029c7eacf
theGeniusBintu <alhassanbintuiddi@gmail.com>
2021-11-03 14:48:54 +0000

    Initial commit

---
+++ src/com/turntabl/Addition.java
@@ -0,0 +1,19 @@
+package com.turntabl;
+
+public class Addition {
+    public int add(int x, int y){
+        return (x + y);
+    }
+
+    public int add(int x, int y, int z){
+        return (x+y+z);
+    }
+
+    public float add(int x, int y, float z){
+       return (x+y+z);
+    }
+
+    public String numbers(String x, String y){
+      return numbers(x,y);
+    }
+}
